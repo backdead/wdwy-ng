5ecbc62741c76010ac6917f00d648dbd461c71bb
Ji Tong <ji.tong@businessdecision.com>
2015-07-08 14:54:45 +0200

    first

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name wdwyNgApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the wdwyNgApp
+ */
+angular.module('wdwyNgApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name wdwyNgApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the wdwyNgApp
+ */
+angular.module('wdwyNgApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
